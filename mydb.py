e2dd697fca07607ba9614e15ac3ce2661b4a1f72
Joy Kalombo <joy__@hotmail.co.uk>
2023-10-21 22:54:05 +0100

    initial commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	passwd = "password123"
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+
+# create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("ALL DONE!")
+# dataBase.close()
